3178cec83759c07025f14e708643f56656b2bfc1
Ethan Eveilleaux <ethan.eveilleaux@sfr.fr>
2025-05-27 11:12:18 +0200

    [WIP] Now use the majority of the Symfony Framework, new background and add a nav for skills

--- public/index.php
+++ public/index.php
@@ -1,81 +1,9 @@
 <?php
 
-// On récupère le chemin absolu du dossier courant
-$currentDir = __DIR__;
+use App\Kernel;
 
-// Détection prod/dev selon la présence de public_html
-if (is_dir(dirname($currentDir) . '/public_html')) {
-    // On est en dev, projet à ../
-    $projectDir = dirname($currentDir).'/..';
-} else {
-    // En prod, projet à ../ppp
-    $projectDir = dirname($currentDir);
-}
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-require_once $projectDir . '/vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Generator\UrlGenerator;
-use Symfony\Bridge\Twig\Extension\RoutingExtension;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
-
-// Importation des routes définies dans src/Router/routes.php
-$routes = require $projectDir . '/src/Router/routes.php';
-
-// Configuration de Twig avec le dossier des templates
-$loader = new FilesystemLoader($projectDir . '/templates');
-$twig = new Environment($loader, [
-    'cache' => $projectDir . '/var/cache/twig',
-    'auto_reload' => true,
-]);
-
-// Configuration du générateur d'URL pour permettre l'utilisation de path() dans Twig
-$urlGenerator = new UrlGenerator($routes, new RequestContext());
-$twig->addExtension(new RoutingExtension($urlGenerator));
-
-// Initialisation du contexte de requête HTTP
-$context = new RequestContext();
-$request = Request::createFromGlobals();
-$context->fromRequest($request);
-
-// Création du routeur (UrlMatcher) pour faire correspondre l'URL à une route
-$matcher = new UrlMatcher($routes, $context);
-
-try {
-    /* START DEBUG ZONE */
-    // $parameters = $matcher->match($request->getPathInfo());
-    // var_dump($parameters);
-    // die();
-    /* END DEBUG ZONE*/
-
-    // Match de la requête avec les routes
-    $parameters = $matcher->match($request->getPathInfo());
-
-    // On injecte les paramètres trouvés dans les attributs de la requête
-    $request->attributes->add($parameters);
-
-    // Si la route utilise un contrôleur (type _controller => NomController::méthode)
-    if (isset($parameters['_controller'])) {
-        [$controllerClass, $method] = explode('::', $parameters['_controller']);
-
-        // Instanciation dynamique du contrôleur
-        $controller = new $controllerClass();
-
-        // Exécution de la méthode du contrôleur avec la requête et Twig
-        $response = $controller->$method($request, $twig);
-    } else {
-        // Sinon on rend un simple template (routes statiques)
-        $response = new Response($twig->render($parameters['template']));
-    }
-
-} catch (Exception $e) {
-    // Si aucune route ne correspond → page 404
-    $response = new Response($twig->render('404.html.twig'), 404);
-}
-
-// Envoi de la réponse HTTP au client
-$response->send();
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
--- src/Controller/StudyController.php
+++ src/Controller/StudyController.php
@@ -2,18 +2,70 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Twig\Environment;
 
-class StudyController
+class StudyController extends AbstractController
 {
-    public function showYear(Request $request, Environment $twig): Response
+    private string $projectDir;
+
+    public function __construct(ParameterBagInterface $params)
     {
-        $year = $request->attributes->get('year');
-        
-        return new Response($twig->render('studies.html.twig', [
-                'year' => $year
-        ]));
+        $this->projectDir = $params->get('kernel.project_dir');
+    }
+
+    /**
+     * Récupère tous les numéros de fichiers skillX.html.twig dans l'année donnée.
+     */
+    private function getSkills(int $year): array
+    {
+        $templateDir = $this->projectDir . '/templates/partials/studies/year' . $year;
+
+        $finder = new Finder();
+        $skillNumbers = [];
+
+        if (is_dir($templateDir)) {
+            $finder->files()->in($templateDir)->name('/^skill\d+\.html\.twig$/');
+
+            foreach ($finder as $file) {
+                if (preg_match('/skill(\d+)\.html\.twig$/', $file->getFilename(), $matches)) {
+                    $skillNumbers[] = (int)$matches[1];
+                }
+            }
+
+            sort($skillNumbers);
+        }
+
+        return $skillNumbers;
+    }
+
+    #[Route('/studies/{year}', name: 'app_studies')]
+    public function showYear(Request $request, int $year): Response
+    {
+        $skills = $this->getSkills($year);
+        $skill = $request->query->getInt('skill', $skills[0] ?? 1);
+
+        return $this->render('studies.html.twig', [
+            'year' => $year,
+            'skill' => $skill,
+            'skills' => $skills,
+        ]);
+    }
+
+    #[Route('/studies/{year}/{skill}', name: 'app_skill_show')]
+    public function showSkill(int $year, int $skill): Response
+    {
+        $skills = $this->getSkills($year);
+
+        return $this->render('studies.html.twig', [
+            'year' => $year,
+            'skill' => $skill,
+            'skills' => $skills,
+        ]);
     }
 }
